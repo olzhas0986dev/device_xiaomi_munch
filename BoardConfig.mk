0c592c8da61976d6f010e049cfb86605379916ce
Rik <aritrarik536@protonmail.com>
2025-03-24 14:55:35 +0530

    munch: Inherit from global firmware

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
+# Inherit from global firmware
+-include vendor/xiaomi/munch-firmware/BoardConfigVendor.mk
+
 DEVICE_PATH := device/xiaomi/munch
 
 # Board
